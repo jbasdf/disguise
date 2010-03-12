f2784a62a413f3459a6391241304944df40f1e15
Justin Ball <justinball@gmail.com>
2010-03-11 23:56:54 -0700

    started rails 3 changes

--- lib/action_controller/disguise_application.rb
+++ lib/action_controller/disguise_application.rb
@@ -1,60 +1,69 @@
 module ActionController
   module DisguiseApplication
     
-    # Module automatically mixed into the all controllers
-    def self.included(base)
-      base.class_eval do
-        include InstanceMethods
-        before_filter :setup_theme
+    extend ActiveSupport::Concern
+    
+    included do
+      before_filter :setup_theme
+      helper_method current_theme
+
+      class_attribute :themes_enabled
+      class_attribute :theme_full_base_path
+      class_attribute :theme_path
+    end
+
+    module ClassMethods
+      def set_theme_path(path = nil, &block)
+        self.theme_path = block || lambda { path }
       end
-      base.send :helper_method, :current_theme
+    end
+
+    def current_theme
+      if DomainTheme.use_domain_themes?
+        @theme ||= DomainTheme.get_theme(request)
+      else
+        @theme ||= Theme.first
+      end
+    end
+
+    protected
+
+    def setup_theme
+      path = instance_eval(&theme_path)
+
+      return if !themes_enabled
+      return if current_theme.blank? || current_theme.name.blank?
+      theme_view_path = File.join(theme_full_base_path, current_theme.name, 'views')
+      # In Rails 3:
+      # FileSystemResolver.new
+      # theme_view_path = File.join(Disguise::Config.theme_full_base_path, current_theme.name, 'views')
+      if self.view_paths.first == theme_view_path
+        return
+      else
+        return if !theme_exists(theme_view_path)
+        clean_theme_view_path
+        self.prepend_view_path(theme_view_path)
+        clean_theme_locale
+        set_theme_locale
+        I18n.reload!
+      end
+    end
+
+    def theme_exists(theme_view_path)
+      @themes_exists ||= {}
+      @themes_exists[theme_view_path] ||= File.exists?(theme_view_path)
     end
     
-    module InstanceMethods
-      
-      def current_theme
-        if DomainTheme.use_domain_themes?
-          @theme ||= DomainTheme.get_theme(request)
-        else
-          @theme ||= Theme.first
-        end
-      end
+    def clean_theme_view_path
+      self.view_paths.delete_if {|view_path| view_path.to_s.index(theme_path) == 0}
+    end
 
-      protected
+    def clean_theme_locale
+      I18n.load_path.delete_if {|localization_path| localization_path.index(theme_full_base_path) == 0}
+    end
 
-      def setup_theme
-        return if !Disguise::Config.themes_enabled
-        return if current_theme.blank? || current_theme.name.blank?
-        theme_view_path = File.join(Disguise::Config.theme_full_base_path, current_theme.name, 'views')
-        if self.view_paths.first == theme_view_path
-          return
-        else
-          return if !theme_exists(theme_view_path)
-          clean_theme_view_path
-          self.prepend_view_path(theme_view_path)
-          clean_theme_locale
-          set_theme_locale
-          I18n.reload!
-        end
-      end
-
-      def theme_exists(theme_view_path)
-        @themes_exists ||= {}
-        @themes_exists[theme_view_path] ||= File.exists?(theme_view_path)
-      end
-      
-      def clean_theme_view_path
-        self.view_paths.delete_if {|view_path| view_path.to_s.index(Disguise::Config.theme_path) == 0}
-      end
-
-      def clean_theme_locale
-        I18n.load_path.delete_if {|localization_path| localization_path.index(Disguise::Config.theme_full_base_path) == 0}
-      end
-
-      def set_theme_locale
-        I18n.load_path += current_theme.locales
-      end
-      
+    def set_theme_locale
+      I18n.load_path += current_theme.locales
     end
     
   end
